d8ca0229a99002efbe6240a789b91b76f22a8559
Aravind NC <aravind_n_c@yahoo.co.in>
2018-04-04 07:54:26 +0530

    Added test.

--- index.js
+++ index.js
@@ -5,109 +5,109 @@
  * @param {input} Rule output from jQuery QueryBuilder
  * @return {object}
  */
-module.exports = function (input) {
-	return {
-		m: convertRule(input)
-	}
+module.exports = function(input) {
+    return {
+        m: convertRule(input)
+    }
 };
 
-function convertRule (input) {
+function convertRule(input) {
     /** Settings **/
-	var defaultCondition = 'AND';
+    var defaultCondition = 'AND';
 
-	/** Private Functions **/
-	var escapeRegExp = function(str) {
-		return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
-	};
+    /** Private Functions **/
+    var escapeRegExp = function(str) {
+        return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+    };
 
-	var mongoOperators = {
-		equal: '$eq',
-		not_equal: '$ne',
-		less: '$lt',
-		less_or_equal: '$lte',
-		greater: '$gt',
-		greater_or_equal: '$gte'
-	}
+    var mongoOperators = {
+        equal: '$eq',
+        not_equal: '$ne',
+        less: '$lt',
+        less_or_equal: '$lte',
+        greater: '$gt',
+        greater_or_equal: '$gte'
+    }
 
-	var mongoOperatorsFns = {
-		equal: function(v) {
-			return {
-				[v[0]]: v[1]
-			};
-		},
-		not_equal: function(v) {
-			return {
-				[v[0]]: v[1]
-			};
-		},
-		less: function(v) {
-			return {
-				[v[0]]: v[1]
-			};
-		},
-		less_or_equal: function(v) {
-			return {
-				[v[0]]: v[1]
-			};
-		},
-		greater: function(v) {
-			return {
-				[v[0]]: v[1]
-			};
-		},
-		greater_or_equal: function(v) {
-			return {
-				[v[0]]: v[1]
-			};
-		}
-	}
+    var mongoOperatorsFns = {
+        equal: function(v) {
+            return {
+                [v[0]]: v[1]
+            };
+        },
+        not_equal: function(v) {
+            return {
+                [v[0]]: v[1]
+            };
+        },
+        less: function(v) {
+            return {
+                [v[0]]: v[1]
+            };
+        },
+        less_or_equal: function(v) {
+            return {
+                [v[0]]: v[1]
+            };
+        },
+        greater: function(v) {
+            return {
+                [v[0]]: v[1]
+            };
+        },
+        greater_or_equal: function(v) {
+            return {
+                [v[0]]: v[1]
+            };
+        }
+    }
 
-	var output = [];
+    var output = [];
 
-	if (!input) {
-		return null;
-	}
+    if (!input) {
+        return null;
+    }
 
-	var self = this;
+    var self = this;
 
-	return (function parse(group) {
+    return (function parse(group) {
 
-		if (!group.condition) {
-			group.condition = defaultCondition;
-		}
+        if (!group.condition) {
+            group.condition = defaultCondition;
+        }
 
-		if (['AND', 'OR'].indexOf(group.condition.toUpperCase()) === -1) {
-			throw new Error('Unable to build rule with condition '+ group.condition);
-		}
+        if (['AND', 'OR'].indexOf(group.condition.toUpperCase()) === -1) {
+            throw new Error('Unable to build rule with condition ' + group.condition);
+        }
 
-		if (!group.rules) {
-			return {};
-		}
+        if (!group.rules) {
+            return {};
+        }
 
-		var parts = [];
+        var parts = [];
 
-		group.rules.forEach(function(rule) {
-			if (rule.rules && rule.rules.length > 0) {
-				parts.push(parse(rule));
-			} else {
-				var expFunction = mongoOperatorsFns[rule.operator];
-				var ruleExpression = {};
+        group.rules.forEach(function(rule) {
+            if (rule.rules && rule.rules.length > 0) {
+                parts.push(parse(rule));
+            } else {
+                var expFunction = mongoOperatorsFns[rule.operator];
+                var ruleExpression = {};
 
-				if (expFunction === undefined) {
-					throw new Error('Unknown rule operation for operator ' + rule.operator);
-				}
+                if (expFunction === undefined) {
+                    throw new Error('Unknown rule operation for operator ' + rule.operator);
+                }
 
-				ruleExpression[mongoOperators[rule.operator]] = expFunction.call(self, [rule.id, rule.value]);
+                ruleExpression[mongoOperators[rule.operator]] = expFunction.call(self, [rule.id, rule.value]);
 
-				parts.push(ruleExpression);
-			}
-		});
+                parts.push(ruleExpression);
+            }
+        });
 
-		var groupExpression = {};
+        var groupExpression = {};
 
-		groupExpression['$' + group.condition.toLowerCase()] = parts;
+        groupExpression['$' + group.condition.toLowerCase()] = parts;
 
-		return (groupExpression);
+        return (groupExpression);
 
-	})(input);
+    })(input);
 }
---
+++ test/test.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var expect = require('chai').expect;
+var ruleConverter = require('../index');
+
+describe('#ruleConverter', function() {
+    it('should convert sample rule', function() {
+        var inputData = {
+            "condition": "AND",
+            "rules": [{
+                    "id": "price",
+                    "field": "price",
+                    "type": "double",
+                    "input": "number",
+                    "operator": "less",
+                    "value": 10.25
+                },
+                {
+                    "condition": "OR",
+                    "rules": [{
+                            "id": "category",
+                            "field": "category",
+                            "type": "integer",
+                            "input": "select",
+                            "operator": "equal",
+                            "value": 2
+                        },
+                        {
+                            "id": "category",
+                            "field": "category",
+                            "type": "integer",
+                            "input": "select",
+                            "operator": "equal",
+                            "value": 1
+                        },
+                        {
+                            "condition": "AND",
+                            "rules": [{
+                                "id": "in_stock",
+                                "field": "in_stock",
+                                "type": "integer",
+                                "input": "radio",
+                                "operator": "equal",
+                                "value": 1
+                            }]
+                        }
+                    ]
+                }
+            ],
+            "valid": true
+        };
+
+        var output = JSON.stringify({
+			"m": {
+				"$and": [{
+					"$lt": {
+						"price": 10.25
+					}
+				}, {
+					"$or": [{
+						"$eq": {
+							"category": 2
+						}
+					}, {
+						"$eq": {
+							"category": 1
+						}
+					}, {
+						"$and": [{
+							"$eq": {
+								"in_stock": 1
+							}
+						}]
+					}]
+				}]
+			}
+		});
+
+        var result = JSON.stringify(ruleConverter(inputData));
+
+        expect(result).to.equal(output);
+    });
+});
